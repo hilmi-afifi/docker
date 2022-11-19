3a38044d8c048276a0421cd87a21a3e1faee8fb1
hilmi-afifi <38133643+hilmi-afifi@users.noreply.github.com>
2022-11-19 10:33:02 +0700

    Create install.sh

---
+++ ubuntu/install.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+sudo apt update -y;
+sudo apt install apt-transport-https ca-certificates curl software-properties-common -y;
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg;
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null;
+sudo apt update -y;
+apt-cache policy docker-ce -y;
+sudo apt install docker-ce -y;
+sudo systemctl enable docker;
+docker info;
+
+mkdir -p ~/.docker/cli-plugins/;
+curl -SL https://github.com/docker/compose/releases/download/v2.3.3/docker-compose-linux-x86_64 -o ~/.docker/cli-plugins/docker-compose;
+chmod +x ~/.docker/cli-plugins/docker-compose;
+docker compose version;
+sudo apt install docker docker-compose -y;
